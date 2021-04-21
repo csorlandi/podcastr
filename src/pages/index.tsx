48e25c3a3ac06dec04b3aa2c7bc0a73c0f4bce79
Claudio Orlandi <claudiosorlandi@gmail.com>
2021-04-21 15:14:17 -0300

    feat(home/all-episodes): implement and style all episodes

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -58,7 +58,42 @@ export default function Home({ latestEpisodes, allEpisodes }: HomeProps) {
         </ul>
       </section>
       <section className={styles.allEpisodes}>
+        <h2>Todos episódios</h2>
 
+        <table cellSpacing={0}>
+            <thead>
+              <th></th>
+              <th>Podcast</th>
+              <th>Integrantes</th>
+              <th>Data</th>
+              <th>Duração</th>
+              <th></th>
+            </thead>
+            <tbody>
+              {allEpisodes.map(episode => (
+                <tr key={episode.id}>
+                  <td className={styles.thumbnail}>
+                    <Image
+                      width={120}
+                      height={120}
+                      src={episode.thumbnail}
+                      alt={episode.title}
+                      objectFit="cover"
+                    />
+                  </td>
+                  <td><a href="">{episode.title}</a></td>
+                  <td>{episode.members}</td>
+                  <td className={styles.publishedAt}>{episode.publishedAt}</td>
+                  <td>{episode.durationAsString}</td>
+                  <td>
+                    <button type="button">
+                      <img src="/play-green.svg" alt="Tocar episódio" />
+                    </button>
+                  </td>
+                </tr>
+              ))}
+            </tbody>
+        </table>
       </section>
     </div>
   )
